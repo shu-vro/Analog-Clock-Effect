f9789dc2da86a329d7afbfa91487520ce4f67cfd
Shirshen Dada <72239897+SDG1060@users.noreply.github.com>
2020-11-30 00:47:02 +0600

    Update Clock.js

--- Clock.js
+++ Clock.js
@@ -1,7 +1,9 @@
+// The clock's variables
 var hr = document.getElementById('hr');
 var mn = document.getElementById('mn');
 var sc = document.getElementById('sc');
 
+// The ticks.
 setInterval(() => {
     var time = new Date();
     let hh = time.getHours() * 30;
@@ -11,3 +13,12 @@ setInterval(() => {
     mn.style.transform = 'rotateZ('+mm+'deg) ';
     sc.style.transform = 'rotateZ('+ss+'deg) ';
 }, 1000);
+
+// Stylings.
+var checkbox = document.querySelector('label');
+var body = document.querySelector('body');
+var clock = document.querySelector('.clock');
+checkbox.addEventListener('click', () => {
+    body.classList.toggle('white');
+    clock.classList.toggle('white');
+})
